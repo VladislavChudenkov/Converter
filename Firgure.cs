128b04fbafe4ae71936a466e97b6d23b0f585d18
Vladislav Chudenkov <isip_v.i.chudenkov@mpt.ru>
2023-11-10 22:42:56 +0300

    ggg

---
+++ Firgure.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    internal class Figure
+    {
+        public string Name { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+
+        public Figure(string name, int width, int height)
+        {
+            Name = name;
+            Width = width;
+            Height = height;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+
+
+namespace Converter
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь до файла:");
+            string filePath = Console.ReadLine();
+
+            Console.WriteLine("Содержимое файла:");
+            string content = File.ReadAllText(filePath);
+            Console.WriteLine(content);
+
+            Console.WriteLine("Нажмите F1 для сохранения файла и Escape для выхода.");
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+            if (keyInfo.Key == ConsoleKey.F1)
+            {
+                SaveFile(filePath, content);
+            }
+            else if (keyInfo.Key == ConsoleKey.Escape)
+            {
+                Console.WriteLine("Выход");
+            }
+        }
+
+        static void SaveFile(string filePath, string content)
+        {
+            string extension = Path.GetExtension(filePath);
+
+            if (extension == ".txt")
+            {
+                File.WriteAllText(filePath, content);
+            }
+            else if (extension == ".json")
+            {
+                var figures = JsonSerializer.Deserialize<List<Figure>>(content);
+                File.WriteAllText(filePath, JsonSerializer.Serialize(figures));
+            }
+            else if (extension == ".xml")
+            {
+                var figures = new List<Figure>();
+                using (StringReader stringReader = new StringReader(content))
+                {
+                    XmlSerializer serializer = new XmlSerializer(figures.GetType());
+                    figures = (List<Figure>)serializer.Deserialize(stringReader);
+                }
+
+                using (StringWriter stringWriter = new StringWriter())
+                {
+                    XmlSerializer serializer = new XmlSerializer(figures.GetType());
+                    serializer.Serialize(stringWriter, figures);
+                    File.WriteAllText(filePath, stringWriter.ToString());
+                }
+            }
+
+            Console.WriteLine("Файл успешно сохранён.");
+        }
+    }
+}
